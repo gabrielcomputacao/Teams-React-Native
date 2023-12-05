29bc5e56963dfea73443669236e7642d4ef79315
gabriel <gabrielcomputacao@outlook.com>
2023-12-05 07:22:56 -0300

    feat:  using styled components

--- Teams/src/screens/Groups/index.tsx
+++ Teams/src/screens/Groups/index.tsx
@@ -1,18 +1,19 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Container, Title } from "./styles";
 
 export default function Groups() {
   return (
-    <View style={styles.container}>
-      <Text>Groups!</Text>
-    </View>
+    <Container>
+      {/*  <View style={styles.container}></View> */}
+      <Title>Groups!</Title>
+    </Container>
   );
 }
 
-const styles = StyleSheet.create({
+/* const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
   },
-});
+}); */
