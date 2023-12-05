ce29e4943b0f19d937d9d24e3000f9fcd53c2258
gabriel <gabrielcomputacao@outlook.com>
2023-12-05 13:54:31 -0300

    feat:  using google fonts

--- Teams/App.tsx
+++ Teams/App.tsx
@@ -1,5 +1,20 @@
 import Groups from "@screens/Groups";
+import { ThemeProvider } from "styled-components";
+import theme from "@theme/index";
+import {
+  useFonts,
+  Roboto_700Bold,
+  Roboto_400Regular,
+} from "@expo-google-fonts/roboto";
+import { ActivityIndicatorBase } from "react-native";
 
 export default function App() {
-  return <Groups />;
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+  return (
+    <ThemeProvider theme={theme}>
+      {/* activeIndicator é um componente de loading */}
+      {fontsLoaded ? <Groups /> : <ActivityIndicatorBase />}
+    </ThemeProvider>
+  );
 }
