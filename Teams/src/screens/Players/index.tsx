d8d2c7bc3b5ce624d8060854cc91db0b139c6454
gabriel <gabrielcomputacao@outlook.com>
2023-12-08 22:34:53 -0300

    feat:  creating PlayerCard and Players component

--- Teams/src/screens/Players/index.tsx
+++ Teams/src/screens/Players/index.tsx
@@ -1,11 +1,19 @@
 import { Header } from "@components/Header";
-import { Container, Form } from "./styles";
+import { Container, Form, HeaderList, NumberOfPlayers } from "./styles";
 import { Highlight } from "@components/Highlight";
 import { ButtonIcon } from "@components/ButtonIcon";
 import { Input } from "@components/Input";
 import { Filter } from "@components/FIlter";
+import { FlatList } from "react-native";
+import { useState } from "react";
+import { PlayerCard } from "@components/PlayerCard";
+import { ListEmpty } from "@components/ListEmpty";
+import { Button } from "@components/Button";
 
 export function Players() {
+  const [team, setTeam] = useState("TimeA");
+  const [players, setPlayers] = useState([]);
+
   return (
     <Container>
       <Header showBackButton />
@@ -18,7 +26,39 @@ export function Players() {
         <ButtonIcon icon="add" type="PRIMARY" />
       </Form>
 
-      <Filter title="Time A" />
+      <HeaderList>
+        <FlatList
+          data={["timeA", "timeB"]}
+          keyExtractor={(item) => item}
+          renderItem={({ item }) => (
+            <Filter
+              title={item}
+              isActive={team === item}
+              onPress={() => setTeam(item)}
+            />
+          )}
+          horizontal
+        />
+
+        <NumberOfPlayers>{players.length}</NumberOfPlayers>
+      </HeaderList>
+      <FlatList
+        data={players}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <PlayerCard name={item} onRemove={() => {}} />
+        )}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Não tem resultados ainda para players" />
+        )}
+        showsVerticalScrollIndicator={false}
+        /* com o array e possivel passar varios estilos que sao aplicados dependendo da condicional  */
+        contentContainerStyle={[
+          { paddingBottom: 100 },
+          players.length === 0 && { flex: 1 },
+        ]}
+      />
+      <Button title="Remover Turma" type="SECONDARY" />
     </Container>
   );
 }
