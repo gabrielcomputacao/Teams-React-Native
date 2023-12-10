020d51b35abbaaaac7f9b57424f48c39e4b3511c
gabriel <gabrielcomputacao@outlook.com>
2023-12-10 07:17:03 -0300

    feat:  creating navigation

---
+++ Teams/src/@types/navigation.d.ts
@@ -0,0 +1,12 @@
+export declare global {
+  namespace ReactNavigation {
+    interface RootParamList {
+      /* nome da rota / se tem parametro ou nao */
+      groups: undefined;
+      new: undefined;
+      players: {
+        group: string;
+      };
+    }
+  }
+}
--- Teams/src/screens/NewGroup/index.tsx
+++ Teams/src/screens/NewGroup/index.tsx
@@ -3,8 +3,13 @@ import { Container, Content, Icon } from "./styles";
 import { Highlight } from "@components/Highlight";
 import { Button } from "@components/Button";
 import { Input } from "@components/Input";
+import { useNavigation } from "@react-navigation/native";
 
 export function NewGroup() {
+  const navigation = useNavigation();
+
+  function handleNewGroup() {}
+
   return (
     <Container>
       <Header showBackButton />
@@ -16,7 +21,11 @@ export function NewGroup() {
 
         <Input placeholder="Nome da turma" />
 
-        <Button title="Criar" style={{ marginTop: 20 }} />
+        <Button
+          title="Criar"
+          style={{ marginTop: 20 }}
+          onPress={handleNewGroup}
+        />
       </Content>
     </Container>
   );
