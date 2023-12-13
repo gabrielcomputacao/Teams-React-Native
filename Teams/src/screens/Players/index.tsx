1d5a25f84188b392942e9142adada4e491bec363
gabriel <gabrielcomputacao@outlook.com>
2023-12-13 06:01:50 -0300

    feat:  feature of remove group

--- Teams/src/screens/Players/index.tsx
+++ Teams/src/screens/Players/index.tsx
@@ -9,12 +9,13 @@ import { useEffect, useRef, useState } from "react";
 import { PlayerCard } from "@components/PlayerCard";
 import { ListEmpty } from "@components/ListEmpty";
 import { Button } from "@components/Button";
-import { useRoute } from "@react-navigation/native";
+import { useNavigation, useRoute } from "@react-navigation/native";
 import { AppError } from "@utils/AppError";
 import { playerAddByGroup } from "@storage/player/playerAddByGroup";
 import { playersGetByGroupAndTeam } from "@storage/player/playerGetByGroupAndTeam";
 import { PlayerStorageDTO } from "@storage/player/PlayerStorageDTO";
 import { playerRemoveByGroup } from "@storage/player/playerRemoveByGroup";
+import { groupRemoveByName } from "@storage/group/groupRemoveByName";
 
 type RoutePArams = {
   group: string;
@@ -25,6 +26,7 @@ export function Players() {
   const [players, setPlayers] = useState<PlayerStorageDTO[]>([]);
   const [newPlayersName, setNewPlayersName] = useState("");
   const newPlayerNameInputRef = useRef<TextInput>(null);
+  const navigation = useNavigation();
 
   const route = useRoute();
   const { group } = route.params as RoutePArams;
@@ -73,6 +75,23 @@ export function Players() {
     } catch (error) {
       Alert.alert("Remover Pessoa", "Não foi posível remove.");
     }
+  }
+
+  async function groupRemove() {
+    try {
+      await groupRemoveByName(group);
+
+      navigation.navigate("groups");
+    } catch (error) {
+      Alert.alert("Remover Gropo", "Algo deu errado");
+    }
+  }
+
+  async function handleGroupRemove() {
+    Alert.alert("Remover", "Deseja Remover o grupo?", [
+      { text: "Não", style: "cancel" },
+      { text: "Sim", onPress: () => groupRemove() },
+    ]);
   }
 
   useEffect(() => {
@@ -136,7 +155,11 @@ export function Players() {
           players.length === 0 && { flex: 1 },
         ]}
       />
-      <Button title="Remover Turma" type="SECONDARY" />
+      <Button
+        title="Remover Turma"
+        type="SECONDARY"
+        onPress={handleGroupRemove}
+      />
     </Container>
   );
 }
